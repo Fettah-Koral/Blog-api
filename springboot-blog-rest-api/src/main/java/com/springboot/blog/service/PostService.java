ae34f0ec70104fcd4a91cf71ab010814c0559cbc
Fettah-Koral <koralfettah@gmail.com>
2023-08-02 22:21:17 +0300

    Added pagination and sorting support

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/service/PostService.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/service/PostService.java
@@ -1,14 +1,13 @@
 package com.springboot.blog.service;
 
-import java.util.List;
-
 import com.springboot.blog.dtos.PostDto;
+import com.springboot.blog.dtos.PostResponse;
 
 public interface PostService {
 	
 	PostDto createPost(PostDto postDto);
 
-	List<PostDto> getAllPosts();
+	PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
 
 	PostDto getPostById(long id);
 
