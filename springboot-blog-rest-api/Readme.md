0c7e181537134de00c207284ee7ee43d40e5cf47
Fettah-Koral <koralfettah@gmail.com>
2023-08-13 07:59:27 +0300

    Added Spring Doc support

--- springboot-blog-rest-api/Readme.md
+++ springboot-blog-rest-api/Readme.md
@@ -1,1 +1,16 @@
-Blog Api Project
+
+
+<img src="src/main/resources/static/images/header.png" alt="header"/>
+<h1></h1>
+
+<img src="src/main/resources/static/images/controllers1.png" alt="controllers1"/>
+
+<h1></h1>
+<img src="src/main/resources/static/images/controllers2.png" alt="controllers2"/>
+
+
+<h3>Contact Me</h3>
+
+<a href="https://www.linkedin.com/in/muhammet-fettah-koral-839910281/">My Linkedin Account</a>
+<br/><br/>
+<p>Email : koralfettah@gmail.com </p>
--- springboot-blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -5,7 +5,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring boot Blog App  Rest APIs",
+				description = "Spring boot Blog App  Rest APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "muhammet fettah koral",
+						email = "koralfettah@gmail.com",
+						url=""
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url=""
+				)
+				
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring boot Blog App Documentation",
+				url = "https://github.com/Fettah-Koral/Blog-api/tree/main/springboot-blog-rest-api"
+		)
+)
 public class SpringbootBlogRestApiApplication {
 
 	@Bean
